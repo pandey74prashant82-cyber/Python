40084219fe856d17c5d3f0162a720162e4893ae0
Prashant Pandey <pandey74prashant82@gmail.com>
2026-01-22 23:04:42 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,20 @@
+#Assignment 1
+#Task 1
+
+
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+
+
+#Task 2
+
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+
+print("Hello",first_name,last_name,"!!! welcome to Python")
